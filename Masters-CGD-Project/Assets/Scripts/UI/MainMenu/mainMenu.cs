2a6f2ecf0f69f6590f2f67b4e35638a5c71ed8f5
Oliver2009 <oliver@lancashire5.me.uk>
2022-10-05 08:26:07 +0100

    finished re-applying y changes

--- Masters-CGD-Project/Assets/Scripts/UI/MainMenu/mainMenu.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/MainMenu/mainMenu.cs
@@ -8,11 +8,30 @@ public class mainMenu : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera rotation;
     [SerializeField] CinemachineVirtualCamera mainMenus;
 
+    public void OnEnable()
+    {
+        CamerSwticher.Register(rotation);
+        CamerSwticher.Register(mainMenus);
+        CamerSwticher.CamPriority(rotation);
+    }
+
+    public void OnDisable()
+    {
+        CamerSwticher.UnRegister(rotation);
+        CamerSwticher.UnRegister(mainMenus);
+    }
+
+
     void Update()
     {
        if(Input.GetKeyDown(KeyCode.Space))
-        {
+       {
+            if (CamerSwticher.IsActiveCamera(rotation))
+            {
+                CamerSwticher.CamPriority(mainMenus);
+                rotation.gameObject.SetActive(false);
+            }
 
-        }
+       }
     }
 }
