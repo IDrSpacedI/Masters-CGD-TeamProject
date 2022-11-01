67865641dd37936a329a54f42ef6d0ef61158aab
Oliver2009 <oliver@lancashire5.me.uk>
2022-11-01 18:04:09 +0000

    Merged Ui in to main branch

--- Masters-CGD-Project/Assets/Scripts/UI/InGame/Pause.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/InGame/Pause.cs
@@ -2,67 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pause : MonoBehaviour
-{
-    public bool pause = false;
-    public GameObject p_Canvas;
-    public GameObject Hud;
-    public GameObject InteractiveText;
-    public GameObject timer;
-    public LevelManager manager;
-    void Update()
-    {
+public class Pause : MonoBehaviour {
 
+    public bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public LevelManager manager;
+
+    public void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (pause)
+            if (GameIsPaused)
             {
-                Time.timeScale = 1;
-                p_Canvas.SetActive(false);
-                Hud.SetActive(true);
-                timer.SetActive(true);
-                InteractiveText.SetActive(true);
-                pause = false;
-                Cursor.visible = false;
-
+                Resume();      
             }
             else
             {
-                Time.timeScale = 0;
-                p_Canvas.SetActive(true);
-                Hud.SetActive(false);
-                timer.SetActive(false);
-                InteractiveText.SetActive(false);
-                pause = true;
-                Cursor.visible = true;
+                Pause_();
             }
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            manager.LoadScene("MainMenu");
-            p_Canvas.SetActive(false);
-            Hud.SetActive(false);
-            timer.SetActive(false);
-            InteractiveText.SetActive(false);
-            Time.timeScale = 1;
-            Cursor.visible = true;
-        }
+    public void Resume()
+    {
+       
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PlayerMovement.disableMovement = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        GameIsPaused = false;
+
+    }
+
+    public void Pause_()
+    {
+        
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        GameIsPaused = true;
+        PlayerMovement.disableMovement = true;
+        Cursor.lockState = CursorLockMode.None;
+
     }
 
 
 
-    public void Pause_Function()
+    public void QuitGame()
     {
-            Time.timeScale = 1;
-            p_Canvas.SetActive(false);
-            Hud.SetActive(true);
-            pause = false;
-            Cursor.visible = false;
-    } 
-
-    public void buttontest()
-    {
-        Debug.Log("button pressed");
     }
 }
