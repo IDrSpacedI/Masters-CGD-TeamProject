e6ab75f97a960c7b28f3ce589b4aed8651781096
Harley <hi189@student.anglia.ac.uk>
2022-11-21 11:12:00 +0000

    endgame scene / texture

---
+++ Masters-CGD-Project/Assets/Scripts/EndGame.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+
+    //bool to activate and diactivate script
+    public bool active = true;
+    public bool action = false;
+
+    public GameObject TextPrompt;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && active == true && action == true)
+        {
+            //load endgame scene
+            SceneManager.LoadScene("EndGame");
+
+        }
+    }
+
+    //sets UI elemt to active when player enters
+    void OnTriggerEnter(Collider collision)
+    {
+        //Debug.Log("TRIGGER!!!!!!!!!!");
+        if (active == true)
+        {
+            TextPrompt.SetActive(true);
+            //Pickup.gameObject.SetActive(true);
+        }
+
+        if (collision.transform.tag == "Player")
+        {
+            //Debug.Log("touching");
+            action = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        // Debug.Log("Exitted");
+        TextPrompt.SetActive(false);
+        action = false;
+    }
+}
