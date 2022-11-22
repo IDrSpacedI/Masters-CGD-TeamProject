00dcae2d76b44685d015cb6d7317f1568465843a
Oliver2009 <oliver@lancashire5.me.uk>
2022-11-22 15:10:21 +0000

    added functionality to end game Scene

--- Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelManager.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelManager.cs
@@ -13,16 +13,28 @@ public class LevelManager : MonoBehaviour
     public GameObject loadScreen;
     public Slider slider;
     public TextMeshProUGUI percent;
-    public GameObject mainMenu;
+    public static LevelManager instance;
+    public int scene_Dex;
+    public GameObject txtTips;
 
     public void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
+        instance = this;
+    }
+
+    public void Update()
+    {
+        if(scene_Dex == 1)
+        {
+            txtTips.SetActive(true);
+        }
     }
     public void LoadLevel(int sceneIndex)
     {
 
         StartCoroutine(LoadAsynchronously(sceneIndex));
+        scene_Dex = sceneIndex;
 
     }
 
@@ -30,7 +42,7 @@ public class LevelManager : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        mainMenu.SetActive(false);
+     
         loadScreen.SetActive(true);
 
         while (!operation.isDone)
