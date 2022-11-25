986a0c65a49200eee608f5250612e14b3d89a3dd
Luke Lloyd <lukelloyd4273@gmail.com>
2022-11-25 22:32:56 +0000

    Added Barricade

---
+++ Masters-CGD-Project/Assets/Scripts/OpernBarricade.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpernBarricade : MonoBehaviour
+{
+    [SerializeField] private GameObject barricadeDoor;
+    private Animator anim;
+
+	private void Awake()
+	{
+        anim = barricadeDoor.GetComponent<Animator>();
+    }
+
+	private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            anim.SetBool("Play", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            anim.SetBool("Play", false);
+        }
+    }
+}
