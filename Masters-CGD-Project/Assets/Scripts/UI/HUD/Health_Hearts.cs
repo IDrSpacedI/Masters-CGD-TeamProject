c8db8b3046785892ee0cb5dae58be7dc2bda9f57
Oliver2009 <oliver@lancashire5.me.uk>
2022-11-29 21:38:35 +0000

    Started to work on settings script & created Heart Ui script

--- Masters-CGD-Project/Assets/Scripts/UI/HUD/Health_Hearts.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/HUD/Health_Hearts.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health_Hearts : MonoBehaviour
 {
+    public HealthSysytem health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+ 
     // Start is called before the first frame update
     void Start()
     {
