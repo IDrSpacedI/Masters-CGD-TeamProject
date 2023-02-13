a70ec0a096e96d3c03f6ed737d4c0c61a4096ab3
Oliver2009 <oliver@lancashire5.me.uk>
2023-02-13 16:36:04 +0000

    updating Main Menu

--- Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelLoadingTest.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelLoadingTest.cs
@@ -2,8 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelLoadingTest : MonoBehaviour
 {
+    public GameObject Loadscreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
 
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncronouly(sceneIndex));
+    }
+
+    public IEnumerator LoadAsyncronouly(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        Loadscreen.SetActive(true);
+       
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
 }
