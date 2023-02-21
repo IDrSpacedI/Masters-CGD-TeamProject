afc139b5519567b8c9d8c3eb97f56d49d7848ccb
melvinpeter <59769386+melvinpeter@users.noreply.github.com>
2023-02-21 16:50:12 +0000

    enemy_bloodeffect

--- Masters-CGD-Project/Assets/Scripts/AI/HealthManagmentNPC.cs
+++ Masters-CGD-Project/Assets/Scripts/AI/HealthManagmentNPC.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthManagmentNPC : MonoBehaviour
 {
     public int health;
+    public GameObject bloodvfx;
 
     //Attack from the outside
     public void attack(int damage)
@@ -16,7 +17,13 @@ public class HealthManagmentNPC : MonoBehaviour
             return;
         }
         health = health - damage;
+        bloodvfx.SetActive(true);
+        Invoke("disableeffect",1);
         FindObjectOfType<SoundManager>().PlaySound("NPCTakingDamage");
         Debug.Log(this.name + " took damage; Current health: " + health);
+    }
+    void disableeffect()
+    {
+        bloodvfx.SetActive(false);
     }
 }
