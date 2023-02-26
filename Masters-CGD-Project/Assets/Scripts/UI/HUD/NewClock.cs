9626d92d2fb28b8cb19bc66b25e6361399665a30
Oliver2009 <oliver@lancashire5.me.uk>
2023-02-26 17:29:17 +0000

    updated new clock script

--- Masters-CGD-Project/Assets/Scripts/UI/HUD/NewClock.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/HUD/NewClock.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewClock : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    TimeManager tm;
+    public RectTransform skyDome;
+    float nighthoursToDegree, dayHoursToDegree;
+
     void Start()
     {
+        tm = FindObjectOfType<TimeManager>();
+
+
+        nighthoursToDegree = 180 / (TimeManager.hoursInDay * tm.nightDuration);
+        dayHoursToDegree = 180 / (TimeManager.hoursInDay * (1 - tm.nightDuration));
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+      if(((tm.getHour() < tm.sunriseHour || tm.getHour() > tm.GetSunset())  && tm.sunriseHour < tm.GetSunset())||
+        ((tm.getHour() < tm.sunriseHour || tm.getHour() > tm.GetSunset()) && tm.sunriseHour > tm.GetSunset()))
+
+
+      {
+            skyDome.Rotate(0, 0, -Time.deltaTime * TimeManager.hoursInDay * nighthoursToDegree / tm.dayDuration);
+      }
+      else
+      {
+            skyDome.Rotate(0, 0, -Time.deltaTime * TimeManager.hoursInDay * dayHoursToDegree / tm.dayDuration);
+      }
     }
 }
