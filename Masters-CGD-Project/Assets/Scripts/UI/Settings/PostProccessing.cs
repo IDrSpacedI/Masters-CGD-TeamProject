0076c0778289b72466477042da2eee5397a3fcff
Oliver2009 <oliver@lancashire5.me.uk>
2023-03-05 20:38:44 +0000

    updated settings script

---
+++ Masters-CGD-Project/Assets/Scripts/UI/Settings/PostProccessing.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProccessing : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume _postprocessVolume;
+    private Bloom _bloom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _postprocessVolume.profile.TryGetSettings(out _bloom);
+    }
+
+   public void
+}
