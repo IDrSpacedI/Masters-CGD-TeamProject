2dd73ca23fa4cd9d55d17121b2f0d536fd79c6ea
Oliver2009 <oliver@lancashire5.me.uk>
2023-03-07 22:34:35 +0000

    updated script

--- Masters-CGD-Project/Assets/Scripts/UI/Settings/PostProccessing.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/Settings/PostProccessing.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.RendererUtils;
+
 
 public class PostProccessing : MonoBehaviour
 {
     [SerializeField] private PostProcessVolume _postprocessVolume;
     private Bloom _bloom;
+    private DepthOfField _depthOfField;
+
     // Start is called before the first frame update
     void Start()
     {
         _postprocessVolume.profile.TryGetSettings(out _bloom);
+        _postprocessVolume.profile.TryGetSettings(out _depthOfField);
+
     }
 
-   
+    public void AmbientOcclusionOff(bool value_B)
+    {
+        _bloom.active = value_B;              
+    }
+
+    public void DepthofFieldOff(bool value_d)
+    {
+        _depthOfField.active = value_d;
+    }
+
+
+
+
+
+
 }
