bb852a65476a7d2f0464ee989546e14369fd0310
Oliver2009 <oliver@lancashire5.me.uk>
2023-03-08 01:41:52 +0000

    updated loading scene

--- Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelManager.cs
+++ Masters-CGD-Project/Assets/Scripts/UI/LoadingScreens/LevelManager.cs
@@ -11,178 +11,89 @@ using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
-    [Header("Reference")]
-    public static LevelManager Instance;
-    [Header("GameObjects")]
-    public GameObject Loadscreen;
-    public GameObject PressSpace;
-    public GameObject sound;
-    public GameObject bar;
-    [Header("bools")]
-    public bool allowGameStart = false;
-    public bool MainLevel;
-    public bool Mainmenu;
-    [Header("UI")]
-    public TextMeshProUGUI tipText;
-    public Image Background;
-    [Header("Arrays")]
-    public string[] tips;
-    public Sprite[] sprites;
-    [Header("Animators")]
-    public Animator P_bar;
-    public Animator text;
-    [Header("Int")]
-    public int x;
+    public static LevelManager levelManager;
+
+    public GameObject loadingPanel;
+
+    public int targetScene;
+
+    public float MinLoadTime;
+    public Image fade_Image;
+    public float fadeTime;
+
+    private bool isLoading;
 
     private void Awake()
     {
-        Instance = this; // static reference
-        allowGameStart = false; // bool set to faluse
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+
+        loadingPanel.SetActive(false);
+        fade_Image.gameObject.SetActive(false);
     }
 
-  /// <summary>
-  /// / hover over to increase font size
-  /// </summary>
-  /// <param name="txt"></param>
-    public void onClickEnter(TextMeshProUGUI txt)
+
+    public void LoadScene(int index)
     {
-        txt.fontSize = 75;
-
-
+        targetScene = index;
+        StartCoroutine(LoadSceneRoutine());
+ 
     }
-
-    public void LoadLevel(int index)
-    {
-        SceneManager.LoadScene(index);
-    }
-
-    /// <summary>
-    /// shrink text slize back to original
-    /// </summary>
-    /// <param name="txt"></param>
-    public void onClickExit(TextMeshProUGUI txt)
-    {
-        txt.fontSize = 70;
-
-
-    }
-    /// <summary>
-    ///  load  back to main scene
-    /// </summary>
-    public void LoadScenes()
-    {
-        StartCoroutine(LoadScene());
-        MainLevel = true;
-    }
-
-    /// <summary>
-    /// load main leve scene
-    /// </summary>
-    public void MainLoadScenes()
-    {
-        StartCoroutine(scene_Load());
-        PlayerMovement.disableMovement = false;
-        Time.timeScale = 1;
-        // stop all sound
-        SoundManager.instance.StopSound("backgroundForest1");
-        SoundManager.instance.StopSound("footStepsGrass");
-        SoundManager.instance.StopSound("backgroundForest2");
-        SoundManager.instance.StopSound("backgroundForest3");
-        SoundManager.instance.StopSound("backgroundForest4");
-        SoundManager.instance.StopSound("Music1");
-        SoundManager.instance.StopSound("coin");
-        SoundManager.instance.StopSound("BackgroundSoft");
-        sound.SetActive(false);
-        Mainmenu = true;
-
-    }
-
-    public void OnApplicationQuit()
-    {
-        Application.Quit();
-    }
-
-    /// <summary>
-    /// load scene main game scene with loading screen
-    /// </summary>
-    /// <returns></returns>
-    public IEnumerator LoadScene()
+    
+    private IEnumerator LoadSceneRoutine()
     {
 
-        x = Random.Range(0, 1);
-        Background.sprite = sprites[x];
-        Loadscreen.SetActive(true);
-        P_bar.Play("Progress");
-        tipText.text = tips[Random.Range(0, tips.Length)];
-        yield return new WaitForSeconds(12f);
-        allowGameStart = true;
-        bar.SetActive(false);
-        tipText.gameObject.SetActive(false);
-        PressSpace.SetActive(true);
-        text.Play("SpaceBar");
-        allowGameStart = true;
-    }
+        isLoading = true;
 
-    /// <summary>
-    /// load scene main game scene with loading screen
-    /// </summary>
-    /// <returns></returns>
-    public IEnumerator scene_Load()
-    {
-        x = Random.Range(0, 1);
-        Background.sprite = sprites[x];
-        Loadscreen.SetActive(true);
-        P_bar.Play("Progress");
-        tipText.text = tips[Random.Range(0, tips.Length)];
-        yield return new WaitForSeconds(12f);
-        bar.SetActive(false);
-        tipText.gameObject.SetActive(false);
-        PressSpace.SetActive(true);
-        text.Play("SpaceBar");
-    }
-    /// <summary>
-    /// Loads mainMenu
-    /// </summary>
-    public void MainMenu()
-    {
-        StartCoroutine(scene_Load());
-        Time.timeScale = 1;
-        Mainmenu = true;
-    }
+        fade_Image.gameObject.SetActive(true);
+        fade_Image.canvasRenderer.SetAlpha(0);
+        while (!Fade(1))
+            yield return null;
 
-    public void Update()
-    {
-        // checks if bools are true
-        if (allowGameStart == true && MainLevel == true)
+
+        loadingPanel.SetActive(true);
+
+        while (!Fade(0))
+            yield return null;
+
+
+       AsyncOperation operation = SceneManager.LoadSceneAsync(targetScene);
+        float elapsedLoadTime = 0f;
+      
+
+        while (!operation.isDone)
         {
-            // activate level load
-            if (Input.GetKey(KeyCode.Space))
-            {
-               
-                SceneManager.LoadScene("MainLevel1");
-
-
-            }
+            elapsedLoadTime += Time.deltaTime;
+            yield return null;
         }
-        // checks is bool is true
-        if(Mainmenu == true)
+
+        while(elapsedLoadTime < MinLoadTime)
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                // activates level loading
-          
-                SceneManager.LoadScene("MainMenu");
-
-
-            }
+            elapsedLoadTime += Time.deltaTime;
+            yield return null;
         }
-        //Debug.Log(allowGameStart);
 
-       
+        while (!Fade(1))
+            yield return null;
+
+
+        loadingPanel.SetActive(false);
+
+        while (!Fade(0))
+            yield return null;
+
+        isLoading = false;
     }
 
+    private bool Fade(float target)
+    {
+        fade_Image.CrossFadeAlpha(target, fadeTime, true);
+
+        if(Mathf.Abs(fade_Image.canvasRenderer.GetAlpha() - target) <= 0.05f)
+        {
+            fade_Image.canvasRenderer.SetAlpha(target);
+            return true;
+        }
+        return false;
+    }
 
 
 }
